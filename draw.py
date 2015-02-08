ed816fa624c07af82149ed732d1cd5fa3aaf0cc9
Wladimir J. van der Laan <laanwj@gmail.com>
2015-02-08 16:19:50 +0100

    visualization changes for data flow tracking

--- draw.py
+++ draw.py
@@ -7,7 +7,7 @@ from __future__ import division, print_function
 import sys
 from collections import defaultdict
 from gi.repository import Gtk, Gdk
-import itertools
+import itertools, operator
 import cairo, math
 import sys
 from collections import defaultdict
@@ -29,6 +29,19 @@ HITBUFFER_W = 600
 HITBUFFER_H = 600
 INITIAL_SCALE = 600
 MOVE_AMOUNT = 200
+# colors for tag bits
+tag_bit_colors = [
+(1.0,0.0,0.0), # red
+(1.0,0.5,0.0), # orange
+(1.0,1.0,0.0), # yellow
+(0.0,1.0,0.0), # green
+(0.0,1.0,1.0), # cyan
+(0.0,0.5,1.0),
+(0.0,0.0,1.0), # blue
+#(0.5,0.0,1.0), #
+(1.0,0.0,1.0), # purple
+#(1.0,0.0,0.5), #
+]
 
 def draw_segs(cr, seg):
     cr.move_to(seg[1], seg[2])
@@ -54,7 +67,7 @@ class MouseButtons:
 
 
 class ChipVisualizer(Gtk.Window):
-    def __init__(self, circuit, overlay_info):
+    def __init__(self, circuit, overlay_info, frames):
         super(ChipVisualizer, self).__init__()
         self.width = 1300
         self.height = 600
@@ -62,6 +75,8 @@ class ChipVisualizer(Gtk.Window):
 
         self.c = circuit
         self.overlay_info = overlay_info
+        self.frames = frames
+        self.frame = 0
         self.cur_overlay = None
         self.hitbuffer_data = None
         self.background = None
@@ -71,6 +86,7 @@ class ChipVisualizer(Gtk.Window):
         self.selection_locked = False
         self.highlighted = None
         self.history = []
+        self.show_extrasel = False
 
         self.ibw = None
         self.ibh = None
@@ -150,14 +166,11 @@ class ChipVisualizer(Gtk.Window):
                 cr.fill()
         return cr.pop_group()
 
-    def draw_selection(self, cr):
+    def draw_selection(self, cr, node_attr):
         cr.save()
         self.perform_transformation(cr)
 
-        cr.set_source_rgba(0.0,1.0,1.0,1.0)
-        for seg in self.c.seg[self.selected]:
-            draw_segs(cr, seg)
-        cr.fill()
+        node_attr[self.selected]['color'] = (0.0,1.0,1.0)
 
         sel_node = self.c.node[self.selected]
 
@@ -175,38 +188,28 @@ class ChipVisualizer(Gtk.Window):
         v_node -= {self.selected, self.c.pwr, self.c.gnd}
         gated -= {self.selected, self.c.pwr, self.c.gnd}
 
-        # local group ("side")
-        for node in v_node:
-            for seg in self.c.seg[node]:
-                draw_segs(cr, seg)
-        cr.set_source_rgba(0.0,0.0,1.0,1.0)
-        cr.fill()
-
-        # direct influence ("downstream")
         gates = set()
         for t in sel_node.gates:
             gates.add(self.c.trans[t].c1)
             gates.add(self.c.trans[t].c2)
         gates -= {self.c.gnd, self.c.pwr}
 
-        cr.set_source_rgba(0.0,0.5,1.0,1.0)
-        for node in gates:
-            for seg in self.c.seg[node]:
-                draw_segs(cr, seg)
-        cr.fill()
-
-        # influenced by ("upstream")
-        cr.set_source_rgba(0.25,0.25,1.0,1.0)
-        for node in gated:
-            for seg in self.c.seg[node]:
-                draw_segs(cr, seg)
-        cr.fill()
+        if self.show_extrasel:
+            # local group ("side")
+            for node in v_node:
+                node_attr[node]['color'] = (0.0,0.0,1.0)
+            # direct influence ("downstream")
+            for node in gates:
+                node_attr[node]['color'] = (0.0,0.5,1.0)
+            # influenced by ("upstream")
+            for node in gated:
+                node_attr[node]['color'] = (0.25,0.25,1.0)
 
         # TODO: add halo/fade/animation
         # http://mxr.mozilla.org/mozilla2.0/source/gfx/thebes/gfxBlur.cpp
         # http://code.google.com/p/infekt/source/browse/trunk/src/lib/cairo_box_blur.cpp
         cr.restore()
-        return {'v_node':v_node, 'gates':gates, 'gated':gated, 'peers':peers}
+        return {'v_node':v_node, 'gates':gates, 'gated':gated, 'peers':peers, 'extraflags':[]}
 
     def show_node_text(self, cr, node):
         '''Show text and return a selection box'''
@@ -220,14 +223,16 @@ class ChipVisualizer(Gtk.Window):
             text = 'GND '
             node = None
         else:
-            if node == self.highlighted:
-                cr.set_source_rgba(0.9,0.9,0.9,1.0)
+            if node in self.node_attr and 'color' in  self.node_attr[node]:
+                cr.set_source_rgb(*self.node_attr[node]['color'])
+            else:
+                cr.set_source_rgb(0.25, 0.25, 0.25)
             text = '%d ' % node
         cr.show_text(text)
         extents = cr.text_extents(text)
         return SelBox(cpt, extents, node)
 
-    def draw_infobox(self, cr, info):
+    def draw_infobox(self, cr, info, extra_sel_flags):
         '''Draw infobox for node mode'''
         cr.rectangle(self.ibx, self.iby, self.ibw, self.ibh)
         cr.set_source_rgba(0.025, 0.025, 0.025, 0.93)
@@ -254,12 +259,17 @@ class ChipVisualizer(Gtk.Window):
             if sel_node.name is not None:
                 cr.set_source_rgb(*base_color)
                 cr.show_text(sel_node.name + ' ')
+
+            flags = []
             if sel_node.flags & NODE_PULLDOWN:
-                cr.set_source_rgba(*layer_colors[3])
-                cr.show_text('- ')
+                flags.append((layer_colors[3], '-'))
             if sel_node.flags & NODE_PULLUP:
-                cr.set_source_rgba(*layer_colors[4])
-                cr.show_text('+ ')
+                flags.append((layer_colors[4], '+'))
+            flags += extra_sel_flags
+
+            for fc,fs in flags:
+                cr.set_source_rgba(*fc)
+                cr.show_text(fs + ' ')
 
             infoy += ldist*1.5
             tb = infoy
@@ -271,14 +281,12 @@ class ChipVisualizer(Gtk.Window):
             infoy += ldist*1
             for (gnode,bnode) in sorted(info['peers']):
                 cr.move_to(infox, infoy)
-                cr.set_source_rgba(0.25,0.25,1.0,1.0)
                 mapping.append(self.show_node_text(cr, gnode))
                 if self.c.node[gnode].name is not None:
                     cr.set_source_rgb(*base_color)
                     cr.show_text(self.c.node[gnode].name)
 
                 cr.move_to(infox+self.ibw/2, infoy)
-                cr.set_source_rgba(0.0,0.0,1.0,1.0)
                 mapping.append(self.show_node_text(cr, bnode))
                 if self.c.node[bnode].name is not None:
                     cr.set_source_rgb(*base_color)
@@ -296,7 +304,6 @@ class ChipVisualizer(Gtk.Window):
             cr.move_to(infox, infoy)
             for bnode in sorted(info['gates']):
                 cr.move_to(infox, infoy)
-                cr.set_source_rgba(0.0,0.5,1.0,1.0)
                 mapping.append(self.show_node_text(cr, bnode))
                 if self.c.node[bnode].name is not None:
                     cr.set_source_rgb(*base_color)
@@ -341,7 +348,6 @@ class ChipVisualizer(Gtk.Window):
 
         for node in group.nodes:
             cr.move_to(infox, infoy)
-            cr.set_source_rgba(0.0,0.0,1.0,1.0)
             mapping.append(self.show_node_text(cr, node))
             if self.c.node[node].name is not None:
                 cr.set_source_rgb(*base_color)
@@ -360,7 +366,6 @@ class ChipVisualizer(Gtk.Window):
         if group.expr is None:
             for node in group.inputs:
                 cr.move_to(infox, infoy)
-                cr.set_source_rgba(0.25,0.25,1.0,1.0)
                 mapping.append(self.show_node_text(cr, node))
                 if self.c.node[node].name is not None:
                     cr.set_source_rgb(*base_color)
@@ -392,7 +397,6 @@ class ChipVisualizer(Gtk.Window):
                 elif isinstance(expr, InvertNode):
                     cr.show_text("not") # alt: U00AC
                 elif isinstance(expr, NodeValNode):
-                    cr.set_source_rgba(0.0,0.5,1.0,1.0)
                     mapping.append(self.show_node_text(cr, expr.id))
                     if self.c.node[expr.id].name is not None:
                         cr.set_source_rgb(*base_color)
@@ -426,7 +430,6 @@ class ChipVisualizer(Gtk.Window):
         cr.move_to(infox, infoy)
         for bnode in group.dependents:
             cr.move_to(infox, infoy)
-            cr.set_source_rgba(0.0,0.5,1.0,1.0)
             mapping.append(self.show_node_text(cr, bnode))
             if self.c.node[bnode].name is not None:
                 cr.set_source_rgb(*base_color)
@@ -437,7 +440,7 @@ class ChipVisualizer(Gtk.Window):
 
         return mapping
 
-    def draw_highlight(self, cr):
+    def draw_highlight(self, cr, node_attr):
         cr.save()
         self.perform_transformation(cr)
 
@@ -446,6 +449,38 @@ class ChipVisualizer(Gtk.Window):
             draw_segs(cr, seg)
         cr.fill()
         cr.restore()
+
+        node_attr[self.highlighted]['color'] = (0.9,0.9,0.9)
+
+    def draw_frames(self, cr, node_attr, extra_sel_flags):
+        d = self.frames[self.frame]
+        for i,tag in enumerate(d['tags']):
+            if tag != 0:
+                color = (0,0,0)
+                numset = 0
+                for bit,c in enumerate(tag_bit_colors):
+                    if tag & (1<<bit):
+                        color = c
+                        numset += 1
+                if numset > 1:
+                    color = (0.7,0.7,0.7) # mixing...
+
+                node_attr[i]['color'] = color
+
+        cr.select_font_face("Arial",
+                  cairo.FONT_SLANT_NORMAL,
+                  cairo.FONT_WEIGHT_NORMAL)
+        cr.set_font_size(15)
+        cr.move_to(10, self.height - 20)
+        cr.set_source_rgb(1.0,1.0,1.0)
+        cr.show_text('Cycle: %i ' % d['cycle'])
+        cr.show_text('PC: %04x' % d['pc'])
+
+        if self.selected is not None:
+            tag = d['tags'][self.selected]
+            for bit,color in enumerate(tag_bit_colors):
+                if tag & (1<<bit):
+                    extra_sel_flags.append((color, str(bit)))
 
     def on_draw(self, wid, cr):
         cr.rectangle(0, 0, self.width, self.height)
@@ -462,6 +497,7 @@ class ChipVisualizer(Gtk.Window):
         cr.rectangle(0, 0, self.width, self.height) #600, 600)
         cr.fill()
 
+        # display overlay, for per-node scalar visualization
         if self.cur_overlay is not None:
             cr.save()
             self.perform_transformation(cr)
@@ -473,15 +509,33 @@ class ChipVisualizer(Gtk.Window):
                     cr.fill()
             cr.restore()
 
+        self.node_attr = defaultdict(dict)
+        extra_sel_flags = []
+        # display animation frames for dataflow analysis
+        if self.frames:
+            self.draw_frames(cr, self.node_attr, extra_sel_flags)
+
         info = None
         if self.selected is not None: # draw selected
-            info = self.draw_selection(cr)
+            info = self.draw_selection(cr, self.node_attr)
 
-        if self.highlighted is not None: # draw highlight
-            self.draw_highlight(cr)
+        # draw highlighted segments
+        for i,attr in self.node_attr.iteritems():
+            cr.save()
+            self.perform_transformation(cr)
+            if 'color' in attr:
+                cr.set_source_rgb(*attr['color'])
+                for seg in self.c.seg[i]:
+                    draw_segs(cr, seg)
+                cr.fill()
+            cr.restore()
+
+        if self.highlighted is not None:
+            self.draw_highlight(cr, self.node_attr)
+
         # Infobox
         if self.infobox_tab == 0:
-            self.infobox_mapping = self.draw_infobox(cr, info)
+            self.infobox_mapping = self.draw_infobox(cr, info, extra_sel_flags)
         elif self.infobox_tab == 1:
             self.infobox_mapping = self.draw_infobox_group(cr, info)
 
@@ -606,12 +660,26 @@ class ChipVisualizer(Gtk.Window):
         if e.string == 'n': # toggle infobox mode
             self.infobox_tab = (self.infobox_tab + 1) % self.infobox_tabs
             self.darea.queue_draw()
+        if e.string == ',' and self.frames:
+            self.frame -= 1
+            if self.frame < 0:
+                self.frame = len(self.frames)-1
+            self.darea.queue_draw()
+        if e.string == '.' and self.frames:
+            self.frame += 1
+            if self.frame >= len(self.frames):
+                self.frame = 0
+            self.darea.queue_draw()
+        if e.string == 'x': # show upstream/downstream links for selected node
+            self.show_extrasel = not self.show_extrasel
+            self.darea.queue_draw()
         if e.keyval == Gdk.KEY_BackSpace:
             try:
                 self.selected = self.history.pop()
                 self.darea.queue_draw()
             except IndexError:
                 pass
+        # TODO: help 'h'/'?' show information about key commands
 
         return True
 
@@ -650,7 +718,18 @@ def main():
         ('Expression complexity', values)
     ]
 
-    app = ChipVisualizer(c, overlay_info)
+    import glob,json
+    frames = []
+    for fn in glob.glob('perfect6502/flow*.json'):
+        try:
+            with open(fn,'rb') as f:
+                d = json.load(f)
+            frames.append(d)
+        except ValueError:
+            pass
+    frames.sort(key=operator.itemgetter('cycle'))
+
+    app = ChipVisualizer(c, overlay_info, frames)
     #app.cur_overlay = 0
     if len(sys.argv)>1:
         app.selected = int(sys.argv[1])
